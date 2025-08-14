f9c684b77b6bbb74c828c5d47493dfab20b2716f
ttttkii913 <dbgkdud821@daum.net>
2025-08-15 01:37:12 +0900

    Test: mail sender test2

--- DongneRing/src/main/java/com/dongnering/global/config/SecurityConfig.java
+++ DongneRing/src/main/java/com/dongnering/global/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                         .requestMatchers("/test").permitAll()
                         .requestMatchers( "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/v1/login/**").permitAll()
+                        .requestMatchers("/image/**").permitAll()
                         .anyRequest().authenticated()   // 그 외 모든 요청은 인증 필요
                 )
                 .build();
