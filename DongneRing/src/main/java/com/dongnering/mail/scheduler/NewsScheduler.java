aba222a140a09e66b78633f9fd15538852cef4bc
hayeong <139212932+ttttkii913@users.noreply.github.com>
2025-08-15 01:21:23 +0900

    Update NewsScheduler.java

--- DongneRing/src/main/java/com/dongnering/mail/scheduler/NewsScheduler.java
+++ DongneRing/src/main/java/com/dongnering/mail/scheduler/NewsScheduler.java
@@ -16,7 +16,7 @@ public class NewsScheduler {
     // TODO: 뉴스 연결
 
     // 매일 오전 7시에 메일 전송 실행
-    @Scheduled(cron = "0 15 1 * * *")
+    @Scheduled(cron = "0 27 1 * * *")
     public void sendDailyNews() {
         memberRepository.findAll().forEach(member -> {
             try {
